efb0f008b331ebb754279e40afb12a1c69e22b9a
John Axel Eriksson <john@insane.se>
2014-02-05 14:05:14 +0100

    Ensure rbenv is loaded

--- run.sh
+++ run.sh
@@ -4,5 +4,8 @@ mkdir -p tmp/sockets
 mkdir -p tmp/pids
 mkdir -p log
 
+source /etc/profile.d/rbenv.sh
+export RBENV_VERSION=2.0.0-p247
+
 bundle install --deployment
 bundle exec unicorn -c unicorn.rb -E development
